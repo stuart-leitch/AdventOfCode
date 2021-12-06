f8fcfa2d1157fd2be936192409a4ecd6cf236d44
Stuart Leitch <stuart.leitch@freeagent.com>
2021-12-06 09:42:05 +0000

    tidy up

--- 2021-ruby/day6/fish.rb
+++ 2021-ruby/day6/fish.rb
@@ -7,20 +7,14 @@ l[0].split(",").each { |f| fish[f.to_i] += 1 }
 
 def next_day(fish)
   fish_next = []
+  8.times { |c| fish_next[c] = fish[c + 1] }
   fish_next[8] = fish[0]
-  fish_next[7] = fish[8]
-  fish_next[6] = fish[7] + fish[0]
-  fish_next[5] = fish[6]
-  fish_next[4] = fish[5]
-  fish_next[3] = fish[4]
-  fish_next[2] = fish[3]
-  fish_next[1] = fish[2]
-  fish_next[0] = fish[1]
+  fish_next[6] += fish[0]
   return fish_next
 end
 
 def summary(day, fish)
-  print "Day #{day}: #{fish}\nTotal fish: #{fish.sum}\n"
+  print "Day #{day}: Total fish: #{fish.sum}\n"
 end
 
 256.times { |d|
