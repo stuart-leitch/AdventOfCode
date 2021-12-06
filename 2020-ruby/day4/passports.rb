5ba149b14f4d983a13dac97d5e188e8633d45964
Stuart Leitch <stuart.leitch@freeagent.com>
2021-12-06 19:54:24 +0000

    2020 d4 pt2 passing :-)

--- 2020-ruby/day4/passports.rb
+++ 2020-ruby/day4/passports.rb
@@ -1,71 +1,137 @@
 input = "input"
-input = "test_input"
+# input = "test_input"
 
 class Passport
-  attr_reader :valid
+  attr_reader :missing_fields, :invalid_fields
 
   def initialize(str_fields)
     @field_string = str_fields
     @fields = {}
-    @valid = true
-    p str_fields
-    @fields = {}
-    str_fields.split(" ").each { |f|
-      k, v = f.split(":")
+    @missing_fields = []
+    @invalid_fields = []
+
+    p @field_string
+
+    @field_string.split(" ").each { |field|
+      k, v = field.split(":")
       @fields[k] = v
     }
-    p @fields
+    # p @fields
   end
 
-  def valid_byr?
+  def validate_num(fieldname, min, max)
+    return true if @fields[fieldname].to_i.between?(min, max)
+
+    @invalid_fields << fieldname
+    return false
   end
 
-  def valid_iyr?
+  def validate_list(fieldname, allowlist)
+    return true if allowlist.include? @fields[fieldname]
+
+    @invalid_fields << fieldname
+    return false
   end
 
-  def valid_eyr?
+  def validate_re(fieldname, expr)
+    return true if @fields[fieldname].match(expr)
+
+    @invalid_fields << fieldname
+    return false
   end
 
-  def valid_hgt?
+  def valid_byr? # four digits; at least 1920 and at most 2002.
+    validate_num("byr", 1920, 2002)
   end
 
-  def valid_hcl?
+  def valid_iyr? # four digits; at least 2010 and at most 2020.
+    validate_num("iyr", 2010, 2020)
+  end
+
+  def valid_eyr? # four digits; at least 2020 and at most 2030.
+    validate_num("eyr", 2020, 2030)
+  end
+
+  def valid_hgt? # a number followed by either cm or in:
+    # If cm, the number must be at least 150 and at most 193.
+    # If in, the number must be at least 59 and at most 76.
+    return false unless (h = @fields["hgt"])
+    if h.match(/\A\d{3}cm\z/)
+      h.chomp("cm")
+      validate_num("hgt", 150, 193)
+    elsif h.match(/\A\d{2}in\z/)
+      h.chomp("in")
+      validate_num("hgt", 59, 76)
+    else
+      @invalid_fields << "hgt"
+      return false
+    end
+  end
+
+  def valid_hcl? # a # followed by exactly six characters 0-9 or a-f.
+    return false unless @fields["hcl"]
+    validate_re("hcl", /\A#[a-f0-9]{6}\z/)
   end
 
   def valid_ecl?
+    valid_eye_colours = ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"]
+    validate_list("ecl", valid_eye_colours)
   end
 
-  def valid_pid?
+  def valid_pid? # a nine-digit number, including leading zeroes.
+    return false unless @fields["pid"]
+    validate_re("pid", /\A\d{9}\z/)
   end
 
   def valid_cid?
+    return true # Hack the system.
   end
 
   def required_fields_present?
     required = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid", "cid"]
     required.delete("cid") # Hacking the system to allow North Pole Credentials to be valid
+    valid = true
 
     required.each { |f|
       unless @field_string.include? f
-        @valid = false
-        print "Missing : #{f}\n"
+        @missing_fields << f
+        valid = false
       end
     }
-    return @valid
+    return valid
+  end
+
+  def passport_valid?
+    valid = true
+    valid = false unless required_fields_present?
+    valid = false unless valid_byr?
+    valid = false unless valid_iyr?
+    valid = false unless valid_eyr?
+    valid = false unless valid_hgt?
+    valid = false unless valid_hcl?
+    valid = false unless valid_ecl?
+    valid = false unless valid_pid?
+    valid = false unless valid_cid?
+    return valid
   end
 end
 
 valid_passports = 0
+invalid_passports = 0
 File.foreach(input, "\n\n") do |p|
-  p.gsub!("\n", " ")
+  p.gsub!("\n", " ").strip!
 
   passport = Passport.new(p)
-  valid = passport.required_fields_present?
-
-  valid_passports += 1 if valid == true
-  print "Passport is valid: #{valid}\n\n"
+  if passport.passport_valid?
+    print "Passport is valid\n\n"
+    valid_passports += 1
+  else
+    print "Passport is NOT valid (missing: #{passport.missing_fields}.  invalid: #{passport.invalid_fields})\n\n"
+    invalid_passports += 1
+  end
 end
-print "Valid Passports: #{valid_passports}\n"
+print "Valid Passports:   #{valid_passports}\n"
+print "Invalid Passports: #{invalid_passports}\n"
 
 # byr (Birth Year)      - four digits; at least 1920 and at most 2002.
 # iyr (Issue Year)      - four digits; at least 2010 and at most 2020.
