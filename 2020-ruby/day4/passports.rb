8310223eff0d36d65f0a54df82d4877f12e02884
Stuart Leitch <stuart.leitch@freeagent.com>
2021-12-06 15:04:20 +0000

    refactor to Passport class

--- 2020-ruby/day4/passports.rb
+++ 2020-ruby/day4/passports.rb
@@ -1,41 +1,71 @@
 input = "input"
 input = "test_input"
 
-# required = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid", "cid"]
-required = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"]
+class Passport
+  attr_reader :valid
+
+  def initialize(str_fields)
+    @field_string = str_fields
+    @fields = {}
+    @valid = true
+    p str_fields
+    @fields = {}
+    str_fields.split(" ").each { |f|
+      k, v = f.split(":")
+      @fields[k] = v
+    }
+    p @fields
+  end
+
+  def valid_byr?
+  end
+
+  def valid_iyr?
+  end
+
+  def valid_eyr?
+  end
+
+  def valid_hgt?
+  end
+
+  def valid_hcl?
+  end
+
+  def valid_ecl?
+  end
+
+  def valid_pid?
+  end
+
+  def valid_cid?
+  end
+
+  def required_fields_present?
+    required = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid", "cid"]
+    required.delete("cid") # Hacking the system to allow North Pole Credentials to be valid
+
+    required.each { |f|
+      unless @field_string.include? f
+        @valid = false
+        print "Missing : #{f}\n"
+      end
+    }
+    return @valid
+  end
+end
 
 valid_passports = 0
 File.foreach(input, "\n\n") do |p|
-  valid = true
-  fields = {}
   p.gsub!("\n", " ")
-  p p
-  p.split(" ").each { |f|
-    k, v = f.split(":")
-    fields[k] = v
-  }
-  p fields
 
-  required.each { |f|
-    unless p.include? f
-      valid = false
-      print "Missing : #{f}\n"
-    end
-  }
+  passport = Passport.new(p)
+  valid = passport.required_fields_present?
 
   valid_passports += 1 if valid == true
   print "Passport is valid: #{valid}\n\n"
 end
 print "Valid Passports: #{valid_passports}\n"
-
-# byr (Birth Year)
-# iyr (Issue Year)
-# eyr (Expiration Year)
-# hgt (Height)
-# hcl (Hair Color)
-# ecl (Eye Color)
-# pid (Passport ID)
-# cid (Country ID)
 
 # byr (Birth Year)      - four digits; at least 1920 and at most 2002.
 # iyr (Issue Year)      - four digits; at least 2010 and at most 2020.
