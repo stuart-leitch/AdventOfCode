671d28592aa38635f1650260efa5c6d8f1d383a7
Stuart Leitch <stuart.leitch@freeagent.com>
2021-12-07 11:19:55 +0000

    tidied up

--- 2021-ruby/day7/crabs.rb
+++ 2021-ruby/day7/crabs.rb
@@ -1,26 +1,10 @@
-input = "input"
-# input = "test_input"
+crabs = File.read("input").split(",").map(&:to_i)
+positions = Array.new(crabs.max + 1, 0)
 
-crabl = File.readlines(input)
-crabl.each do |crabs|
-  # crabs.to_a
-  # ca = Array.new(crabs)
-  ca = crabs.split(",").map(&:to_i)
-  p ca
-  p ca.min
-  p ca.max
-  poss = Array.new(ca.max + 1, 0)
-  ca.each do |c|
-    (ca.min..ca.max).each do |pos|
-      move = (c - pos).abs
-      cost = (move * (move + 1)) / 2
-      poss[pos] += cost
-    end
+crabs.each do |crab|
+  (0..crabs.max).each do |pos|
+    move = (crab - pos).abs
+    positions[pos] += (move * (move + 1)) / 2
   end
-  p poss
-  p poss.min
-  p poss.index(poss.min)
-  #  do |crab|
-  #   print "#{crab}\n"
-  # end
 end
+print "Minimum fuel is: #{positions.min} at position #{positions.index(positions.min)}\n"
