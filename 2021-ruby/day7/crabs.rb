f6b0d6c2b7d498e41d4d4136b501c95a39ca3526
Stuart Leitch <stuart.leitch@freeagent.com>
2021-12-07 09:11:29 +0000

    d7p1 done

---
+++ 2021-ruby/day7/crabs.rb
@@ -0,0 +1,25 @@
+input = "input"
+input = "test_input"
+
+crabl = File.readlines(input)
+crabl.each do |crabs|
+  # crabs.to_a
+  # ca = Array.new(crabs)
+  ca = crabs.split(",").map(&:to_i)
+  p ca
+  p ca.min
+  p ca.max
+  poss = Array.new(ca.max + 1, 0)
+  ca.each do |c|
+    (ca.min..ca.max).each do |pos|
+      move = (c - pos).abs
+      poss[pos] += move
+    end
+  end
+  p poss
+  p poss.min
+  p poss.index(poss.min)
+  #  do |crab|
+  #   print "#{crab}\n"
+  # end
+end
