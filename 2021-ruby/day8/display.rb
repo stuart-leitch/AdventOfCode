2b698a4351708dd9916ef27dfe4bf1ca74e5d3ff
Stuart Leitch <stuart.leitch@freeagent.com>
2021-12-08 23:05:06 +0000

    monkeypatch String#contains to improve readability

--- 2021-ruby/day8/display.rb
+++ 2021-ruby/day8/display.rb
@@ -1,7 +1,9 @@
 require "set"
 
-def contains?(set, subset)
-  subset.chars.to_set.subset?(set.chars.to_set)
+class String
+  def contains?(substring)
+    substring.chars.to_set.subset?(self.chars.to_set)
+  end
 end
 
 def decode(code)
@@ -10,19 +12,19 @@ def decode(code)
   obs = obs.split(" ").map { |e| e.chars.sort.join }.sort_by { |s| s.length }
   output = output.split(" ").map { |e| e.chars.sort.join }
 
-  codes = [] # Beware! Order matters (i.e. to find code for '9' requires code for '4')
+  codes = [] # Beware! Order matters (i.e. to find code for '2' requires code for '9')
   codes[1] = obs.find { |e| e.length == 2 }
   codes[4] = obs.find { |e| e.length == 4 }
   codes[7] = obs.find { |e| e.length == 3 }
   codes[8] = obs.find { |e| e.length == 7 }
-  codes[9] = obs.find { |e| e.length == 6 && contains?(e, codes[4]) }
-  codes[3] = obs.find { |e| e.length == 5 && contains?(e, codes[1]) }
-  codes[6] = obs.find { |e| e.length == 6 && !contains?(e, codes[1]) }
-  codes[0] = obs.find { |e| e.length == 6 && contains?(e, codes[1]) && !contains?(e, codes[4]) }
-  codes[2] = obs.find { |e| e.length == 5 && !contains?(codes[9], e) }
-  codes[5] = obs.find { |e| e.length == 5 && contains?(codes[9], e) && !contains?(e, codes[1]) }
+  codes[9] = obs.find { |e| e.length == 6 && e.contains?(codes[4]) }
+  codes[3] = obs.find { |e| e.length == 5 && e.contains?(codes[1]) }
+  codes[6] = obs.find { |e| e.length == 6 && !e.contains?(codes[1]) }
+  codes[0] = obs.find { |e| e.length == 6 && e.contains?(codes[1]) && !e.contains?(codes[4]) }
+  codes[2] = obs.find { |e| e.length == 5 && !codes[9].contains?(e) }
+  codes[5] = obs.find { |e| e.length == 5 && codes[9].contains?(e) && !e.contains?(codes[1]) }
 
-  return output.map { |e| codes.index(e.chars.sort.join) }.join.to_i
+  return output.map { |e| codes.index(e) }.join.to_i
 end
 
 print "Part1: "
