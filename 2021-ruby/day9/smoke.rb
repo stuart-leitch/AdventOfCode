8b6d20a571286c02ce29034cfab96986e2e7943f
Stuart Leitch <stuart.leitch@freeagent.com>
2021-12-09 16:13:10 +0000

    d9p2 solved

--- 2021-ruby/day9/smoke.rb
+++ 2021-ruby/day9/smoke.rb
@@ -1,14 +1,12 @@
-$readings = File.readlines("input").map(&:strip).map(&:chars)
-# .map(&:toi)
-# clines = lines.length
+require "set"
 
-$cols = $readings[0].length
-$rows = $readings.length
+$readings = File.readlines("input").map(&:strip).map(&:chars)
 
 def get_val(r, c)
-  # print "get_val: #{r},#{c} (#{$rows},#{$cols})\n"
-  return nil if r >= $rows || r < 0 || c >= $cols || c < 0
-  $readings[r][c]
+  cols = $readings[0].length
+  rows = $readings.length
+  return nil if r >= rows || r < 0 || c >= cols || c < 0
+  $readings[r][c].to_i
 end
 
 def get_vals(r, c)
@@ -20,19 +18,54 @@ def get_vals(r, c)
   return vals
 end
 
-print "\n"
-risk = 0
-$rows.times do |r|
-  # print "ROW: #{r}:\n"
-  $cols.times do |c|
-    # print "c:#{c}: #{$readings[r][c]}: "
-    # print "#{get_vals(r, c)}"
-    if $readings[r][c].to_i < get_vals(r, c).compact.map(&:to_i).min
-      risk += (1 + $readings[r][c].to_i)
-      # print " -- MIN!"
-    end
-    # print "\n"
-  end
-  # print "\n"
+def find_basins()
+  basins = []
+  $readings.each_with_index { |row, r|
+    row.each_with_index { |e, c|
+      if e.to_i < get_vals(r, c).compact.min
+        basins << [r, c]
+      end
+    }
+  }
+  return basins
 end
-p risk
+
+def is_basin?(r, c)
+  height = get_val(r, c)
+  height != nil && height != 9
+end
+
+def extend_basin(basin)
+  tb = Set.new()
+  basin.each do |p|
+    r, c = p[0], p[1]
+    tb << [r - 1, c] if is_basin?(r - 1, c)
+    tb << [r + 1, c] if is_basin?(r + 1, c)
+    tb << [r, c - 1] if is_basin?(r, c - 1)
+    tb << [r, c + 1] if is_basin?(r, c + 1)
+  end
+  sizewas = basin.length
+  basin.merge(tb)
+  sizeis = basin.length
+  extend_basin(basin) if sizeis > sizewas
+  return tb
+end
+
+def get_basin_size(r, c)
+  print "basin: #{r},#{c} -- "
+  basin = Set.new()
+  basin << [r, c]
+  extend_basin(basin)
+  print "#{basin} (#{basin.length})\n"
+  return basin.length
+end
+
+# Part 1
+p basins = find_basins
+p basins.length
+p basins.each.map { |b| get_val(b[0], b[1]) }.reduce(0) { |c, e| c + 1 + e }
+
+# Part 2
+p basins.each.map { |b| get_basin_size(b[0], b[1]) }
+    .sort.reverse.slice(0, 3)
+    .reduce(1) { |sum, num| sum * num }
