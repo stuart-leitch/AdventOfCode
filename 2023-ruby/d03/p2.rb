8a3062f2848e8dd2139a52cef2bfa767773df062
Stuart Leitch <stuart.leitch@leitches.net>
2023-12-04 08:36:56 +0000

    d3p2 tidy up

--- 2023-ruby/d03/p2.rb
+++ 2023-ruby/d03/p2.rb
@@ -1,21 +1,9 @@
 # frozen_string_literal: true
 
-symbols = {}
-File.open(ARGV[0]).readlines.map(&:chomp).each.with_index(1) do |line, line_num|
-  print "#{line_num.to_s.rjust(7, ' ')}: #{line}\n"
-  chars = line.chars
-  chars.each.with_index(1) do |char, char_num|
-    symbols[[char_num, line_num]] = char unless char == '.' || char =~ /[[:digit:]]/
-  end
-end
-pp symbols
-symbols.filter! { |_k, v| v == '*' }
-pp symbols
-
+stars = {}
 numbers = []
 File.open(ARGV[0]).readlines.map(&:chomp).each.with_index(1) do |line, line_num|
-  print "#{line_num.to_s.rjust(7, ' ')}: #{line}\n"
-  chars = line.chars << '.' # add a dummy char to the end of the line
+  chars = line.chars << '.' # add a dummy char to the end of the line (Woff hack)
   num = []
   adj = []
   chars.each.with_index(1) do |char, char_num|
@@ -24,43 +12,29 @@ File.open(ARGV[0]).readlines.map(&:chomp).each.with_index(1) do |line, line_num|
       adj << [char_num - 1, line_num - 1]
       adj << [char_num - 1, line_num]
       adj << [char_num - 1, line_num + 1]
-
       adj << [char_num, line_num - 1]
-      # adj << [char_num, line_num]
       adj << [char_num, line_num + 1]
-
       adj << [char_num + 1, line_num - 1]
       adj << [char_num + 1, line_num]
       adj << [char_num + 1, line_num + 1]
-    elsif num.length > 0
+    elsif !num.empty? || char == '*'
+      stars[[char_num, line_num]] = [] if char == '*'
       numbers << [num.join.to_i, adj.uniq]
-      num = []
-      adj = []
+      num.clear
+      adj.clear
     end
   end
 end
-pp numbers
-
-gears = {}
-symbols.each do |k, _v|
-  gears.store(k, [])
-end
 
 numbers.each do |num|
-  partnum = num[0]
-  adj = num[1]
-  adj.each do |a|
-    gears[a] << partnum if symbols[a] == '*'
+  num[1].each do |a|
+    next if stars[a].nil?
+
+    stars[a] << num[0]
   end
 end
-pp gears
 
-ratio_sum = 0
-gears.each_value do |v|
-  next unless v.length == 2
+gears = stars.filter { |_k, v| v.length == 2 }
 
-  print "#{v}\n"
-  p ratio = v[0] * v[1]
-  ratio_sum += ratio
-end
+ratio_sum = gears.reduce(0) { |acc, (_k, v)| acc + v[0] * v[1] }
 p ratio_sum
